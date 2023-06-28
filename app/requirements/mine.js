36af8e6b05d8d21c83ac14b42ad7d2a826b51fc9
gasanmartina <86533998+gasanmartina@users.noreply.github.com>
2023-06-28 01:30:01 -0400

    feat: subdivide on status my requirements

--- app/requirements/mine.js
+++ app/requirements/mine.js
@@ -7,6 +7,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 const requirements = () => {
 	const [user, setUser] = useState({});
 	const [requirements, setRequirements] = useState([]);
+	const estados = ['activo', 'en subasta', 'en camino', 'finalizado'];
 	const router = useRouter();
 
 	const getActiveRequirements = async () => {
@@ -83,94 +84,61 @@ const requirements = () => {
 		}
 	}, [user]);
 
+	const filterRequirements = (status) => {
+		return requirements.filter((requirement) => requirement.estado === status);
+	}
 
-	return(
-		<View
-			style={styles.container}
-		>
-			<Text
-				style={styles.title}
-			>
-				{user?.rol === 'Cliente externo' &&  "Mis requerimientos" }
-				{user?.rol === 'Productor' &&  "Requerimientos activos" }
+
+	return (
+		<View style={styles.container}>
+			<Text style={styles.title}>
+				{user?.rol === 'Cliente externo' && "Mis requerimientos"}
+				{user?.rol === 'Productor' && "Requerimientos activos"}
 			</Text>
-			<View
-				style={styles.requirements}
-			>
-			{requirements.length === 0 && <Text style={styles.text}>No hay requerimientos</Text>}
+			<View style={styles.requirements}>
+				{requirements.length === 0 && <Text style={styles.text}>No hay requerimientos</Text>}
+				{estados.map((estado) => (
+					<View key={estado}
+						style={{
+							flex: 1,
+							backgroundColor: '#1e2124',
+							padding: 10,
+							gap: 10,
+							height: '100%',
 
-			{requirements.map((requirement) => (
-				<TouchableOpacity
-					style={styles.requirement}
-					key={requirement.id_requerimiento}
-					onPress={() => router.push(`/requirements/${requirement.id_requerimiento}`)}
-				>
-					<View
-						style={styles.field}
+						}}
 					>
-						<Text
-							style={styles.fieldTitle}
-						>
-							Numero
-						</Text>
-						<Text
-							style={styles.text}
-						>
-							{requirement.id_requerimiento}
-						</Text>
+						<Text style={styles.text}>Requerimientos {estado}</Text>
+						{filterRequirements(estado).map((requirement) => (
+							<TouchableOpacity
+								style={styles.requirement}
+								key={requirement.id_requerimiento}
+								onPress={() => router.push(`/requirements/${requirement.id_requerimiento}`)}
+							>
+								<View style={styles.field}>
+									<Text style={styles.fieldTitle}>Numero</Text>
+									<Text style={styles.text}>{requirement.id_requerimiento}</Text>
+								</View>
+								<View style={styles.field}>
+									<Text style={styles.fieldTitle}>Fecha de inicio</Text>
+									<Text style={styles.text}>{formatDate(requirement.fecha_inicio)}</Text>
+								</View>
+								<View style={styles.field}>
+									<Text style={styles.fieldTitle}>Fecha termino</Text>
+									<Text style={styles.text}>{formatDate(requirement.fecha_fin)}</Text>
+								</View>
+								<View style={styles.field}>
+									<Text style={styles.fieldTitle}>Estado</Text>
+									<Text style={styles.text}>{requirement.estado}</Text>
+								</View>
+								<Text style={styles.text}>Ver</Text>
+							</TouchableOpacity>
+						))}
 					</View>
-					<View
-						style={styles.field}
-					>
-						<Text
-							style={styles.fieldTitle}
-						>
-							Fecha de inicio
-						</Text>
-						<Text
-							style={styles.text}
-						>
-							{formatDate(requirement.fecha_inicio)}
-						</Text>
-					</View>
-					<View
-						style={styles.field}
-					>
-						<Text
-							style={styles.fieldTitle}
-						>
-							Fecha termino
-						</Text>
-						<Text
-							style={styles.text}
-						>
-							{formatDate(requirement.fecha_fin)}
-						</Text>
-					</View>
-					<View
-						style={styles.field}
-					>
-						<Text
-							style={styles.fieldTitle}
-						>
-							Estado
-						</Text>
-						<Text
-							style={styles.text}
-						>
-							{requirement.estado}
-						</Text>
-					</View>
-					<Text
-						style={styles.text}
-					>
-						Ver
-					</Text>
-				</TouchableOpacity>
-			))}
+				))}
 			</View>
 		</View>
-	)
+	);
 }
 
 const styles = StyleSheet.create({
@@ -190,7 +158,7 @@ const styles = StyleSheet.create({
 		backgroundColor: '#1e2124',
 		padding: 10,
 		gap: 10,
-		maxHeight: 120,
+		height: '100%',
 	},
 	requirement: {
 		flex: 1,
@@ -200,6 +168,7 @@ const styles = StyleSheet.create({
 		padding: 10,
 		gap: 10,
 		borderRadius: 10,
+		maxHeight: 120,
 	},
 	field: {
 		flex: 1,
