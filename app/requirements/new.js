ff185b673a642a1761485c2f3676500b7257ffef
gasanmartina <86533998+gasanmartina@users.noreply.github.com>
2023-07-06 20:52:49 -0400

    add validations to new requriement

--- app/requirements/new.js
+++ app/requirements/new.js
@@ -1,10 +1,41 @@
 import { View, Text, StyleSheet, TextInput, TouchableOpacity, Platform, ScrollView } from 'react-native';
 import {Picker} from '@react-native-picker/picker';
 import DateTimePicker from '@react-native-community/datetimepicker';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useRouter } from "expo-router";
+
+const calidad_items = [
+  { name: 'Fresca', value:'Fresca' },
+  { name: 'Madura', value:'Madura' },
+  { name: 'Dulce', value:'Dulce' },
+  { name: 'Sabrosa', value:'Sabrosa'},
+  { name: 'Jugosa', value:'Jugosa'}
+]
+
+const productos_items = [
+  { name: 'Manzana', value: 'Manzana' },
+  { name: 'Plátano', value: 'Plátano' },
+  { name: 'Naranja', value: 'Naranja' },
+  { name: 'Piña', value: 'Piña' },
+  { name: 'Fresa', value: 'Fresa' },
+  { name: 'Uva', value: 'Uva' },
+  { name: 'Mango', value: 'Mango' },
+  { name: 'Sandía', value: 'Sandía' },
+  { name: 'Cereza', value: 'Cereza' },
+  { name: 'Pera', value: 'Pera' },
+  { name: 'Melón', value: 'Melón' },
+  { name: 'Mandarina', value: 'Mandarina' },
+  { name: 'Coco', value: 'Coco' },
+  { name: 'Papaya', value: 'Papaya' },
+  { name: 'Limón', value: 'Limón' },
+  { name: 'Fruta de la pasión', value: 'Fruta de la pasión' },
+  { name: 'Granada', value: 'Granada' },
+  { name: 'Kiwi', value: 'Kiwi' },
+  { name: 'Ciruela', value: 'Ciruela' },
+  { name: 'Guayaba', value: 'Guayaba' }
+  ];
 
 const formatDate = (date) => {
 	const year = date.getFullYear();
@@ -19,11 +50,14 @@ const newRequirement = () => {
   const [direccion, setDireccion] = useState('');
   const [showInicio, setShowInicio] = useState(false);
   const [showTermino, setShowTermino] = useState(false);
+  const [validProducts, setValidProducts] = useState(allProductosValid);
 	const [productos, setProductos] = useState([
-		{ nombre: '', cantidad: 1, calidad: '' },
+		{ nombre: productos_items[0].value, cantidad: 0, calidad: calidad_items[0].value },
 	]);
   const [mode, setMode] = useState('date');
   const [show, setShow] = useState(false);
+  const [isButtonEnabled, setIsButtonEnabled] = useState(false);
+
 
 	const router = useRouter();
 
@@ -37,6 +71,23 @@ const newRequirement = () => {
     }
   };
 
+  const allProductosValid = () => {
+    let valid = true;
+
+    productos.forEach((producto) => {
+      if (producto.nombre.length <= 0 || producto.cantidad < 1 || producto.calidad.length <= 0) {
+        valid = false;
+      }
+    });
+
+    return valid;
+  };
+
+  useEffect(() => {
+    console.log(allProductosValid() && direccion.length > 0)
+    setIsButtonEnabled(allProductosValid() && direccion.length > 0);
+  }, [productos, direccion]);
+
   const showDatepickerInicio = () => {
     setShowInicio(true);
   };
@@ -49,18 +100,6 @@ const newRequirement = () => {
 
   const showDatepickerTermino = () => {
     setShowTermino(true);
-  };
-
-
-  const showMode = (currentMode) => {
-    if (Platform.OS === 'android') {
-      setShow(true);
-    }
-    setMode(currentMode);
-  };
-
-  const showDatepicker = () => {
-    showMode('date');
   };
 
 
@@ -134,40 +173,67 @@ const newRequirement = () => {
           onChange={onChangeTermino}
         />
       )}
-      <Text style={styles.subtitle}>Dirección de envio</Text>
-      <TextInput style={styles.textInput} placeholder="Dirección de envio" value={direccion} onChangeText={setDireccion} />
-			<Text
-				style={styles.title}
-			>Productos</Text>
-			   <ScrollView>
       <View
         style={{
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          gap: 10,
+          flex: 1,
+          alignItems: 'center',
+          width: '100%',
+          marginTop: 20
         }}
       >
-        <TouchableOpacity
-          style={styles.addButton}
-          onPress={() => {
-            const newProductos = [...productos];
-            newProductos.push({ nombre: '', cantidad: 1, calidad: '' });
-            setProductos(newProductos);
-          }}
-        >
-          <Text style={styles.addButtonText}>Agregar producto</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.deleteButton}
-          onPress={() => {
-            const newProductos = [...productos];
-            newProductos.pop();
-            setProductos(newProductos);
-          }}
-        >
-          <Text style={styles.deleteButtonText}>Eliminar producto</Text>
-        </TouchableOpacity>
+        <Text style={styles.subtitle}>Dirección de envio</Text>
+        <TextInput
+          style={[styles.textInput, {
+            width: '75%',
+          }]}
+          placeholder="Dirección de envio"
+          placeholderTextColor={"gray"}
+          value={direccion}
+          onChangeText={setDireccion}
+        />
       </View>
+      <View
+        style={{
+          marginTop: 40,
+        }}
+      >
+        <Text
+          style={styles.title}
+        >
+          Productos
+        </Text>
+      </View>
+			<ScrollView>
+        <View
+          style={{
+            flex: 1,
+            flexDirection: 'row',
+            justifyContent: 'center',
+            gap: 10,
+            marginBottom: 10,
+          }}
+        >
+          <TouchableOpacity
+            style={styles.addButton}
+            onPress={() => {
+              const newProductos = [...productos];
+              newProductos.push({ nombre: productos_items[0].value, cantidad: 0, calidad: calidad_items[0].value });
+              setProductos(newProductos);
+            }}
+          >
+            <Text style={styles.addButtonText}>Agregar producto</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.deleteButton}
+            onPress={() => {
+              const newProductos = [...productos];
+              newProductos.pop();
+              setProductos(newProductos);
+            }}
+          >
+            <Text style={styles.deleteButtonText}>Eliminar producto</Text>
+          </TouchableOpacity>
+        </View>
 
       {productos.map((producto, index) => (
         <View
@@ -176,6 +242,7 @@ const newRequirement = () => {
             flexDirection: 'row',
             justifyContent: 'space-between',
             gap: 10,
+            margin: 10,
           }}
         >
           <View
@@ -184,50 +251,42 @@ const newRequirement = () => {
 
             }}
           >
-            <Text style={styles.subtitle}>Producto {index + 1}</Text>
-            <Text style={styles.subtitle}> { producto.nombre } </Text>
-            <View>
-              <Picker
-                selectedValue={producto.nombre}
-                onValueChange={(itemValue, itemIndex) =>{
-                  const newProductos = [...productos];
-                  newProductos[index].nombre = itemValue;
-                  setProductos(newProductos);
-                }}>
-                <Picker.Item label="Manzana" value="Manzana" />
-                <Picker.Item label="Plátano" value="Plátano" />
-                <Picker.Item label="Naranja" value="Naranja" />
-                <Picker.Item label="Fresa" value="Fresa" />
-                <Picker.Item label="Sandía" value="Sandía" />
-                <Picker.Item label="Uva" value="Uva" />
-                <Picker.Item label="Pera" value="Pera" />
-                <Picker.Item label="Piña" value="Piña" />
-                <Picker.Item label="Melón" value="Melón" />
-                <Picker.Item label="Mango" value="Mango" />
-                <Picker.Item label="Papaya" value="Papaya" />
-                <Picker.Item label="Kiwi" value="Kiwi" />
-                <Picker.Item label="Cereza" value="Cereza" />
-                <Picker.Item label="Limón" value="Limón" />
-                <Picker.Item label="Mandarina" value="Mandarina" />
-                <Picker.Item label="Pepino" value="Pepino" />
-                <Picker.Item label="Zanahoria" value="Zanahoria" />
-                <Picker.Item label="Lechuga" value="Lechuga" />
-                <Picker.Item label="Espinaca" value="Espinaca" />
-                <Picker.Item label="Calabacín" value="Calabacín" />
-              </Picker>
-          </View>
+            <Text style={[styles.subtitle, { textAlign: 'center' }]}>Producto {index + 1}
+            </Text>
+            <Picker
+              style={{ height: 50, width: 150, color: "white", textAlign: 'center' }}
+              itemStyle={{ color: 'white', backgroundColor: 'transparent' }}
+              dropdownIconColor={"white"}
+              selectedValue={productos[index].nombre}
+              onValueChange={(itemValue, itemIndex) =>{
+                const newProductos = [...productos];
+                newProductos[index].nombre = itemValue;
+                setProductos(newProductos);
+              }}>
+                {productos_items.map((item, idx) => {
+                  return (
+                    <Picker.Item key={idx} label={item.name} value={item.value} />
+                  );
+                })}
+            </Picker>
           </View>
           <View>
-            <Text style={styles.subtitle}>Cantidad</Text>
+            <Text style={[styles.subtitle, { textAlign: 'center' }]}>Cantidad</Text>
             <TextInput
               style={styles.textInput}
               placeholder="Cantidad"
               value={producto.cantidad.toString()}
               onChangeText={text => {
+                // Remove leading zeros
+                const numericText = text.replace(/^0+(0$|[^0])/, '$1');
+                // Ensure quantity is greater than 1
+                const quantity = parseInt(numericText, 10);
+                const validQuantity = isNaN(quantity) || quantity < 1 ? 1 : quantity;
                 const newProductos = [...productos];
-                newProductos[index].cantidad = text;
+                newProductos[index].cantidad = validQuantity;
                 setProductos(newProductos);
               }}
+              keyboardType="numeric"
             />
           </View>
           <View
@@ -236,20 +295,25 @@ const newRequirement = () => {
 
             }}
           >
-            <Text style={styles.title}>Calidad</Text>
-            <Text style={styles.subtitle}> { producto.calidad } </Text>
+            <Text style={[styles.subtitle, { textAlign: 'center' }]}>Calidad</Text>
             <Picker
-              selectedValue={producto.calidad}
+              style={{
+                height: 50,
+                width: 150,
+                color: "white"
+              }}
+              dropdownIconColor={"white"}
+              selectedValue={productos[index].cantidad}
               onValueChange={(itemValue, itemIndex) =>{
                 const newProductos = [...productos];
                 newProductos[index].calidad = itemValue;
                 setProductos(newProductos);
               }}>
-              <Picker.Item label="Fresca" value="Fresca" />
-              <Picker.Item label="Madura" value="Madura" />
-              <Picker.Item label="Dulce" value="Dulce" />
-              <Picker.Item label="Sabrosa" value="Sabrosa" />
-              <Picker.Item label="Jugosa" value="Jugosa" />
+                {calidad_items.map((item, idx) => {
+                  return (
+                    <Picker.Item key={idx} label={item.name} value={item.value} />
+                  );
+                })}
             </Picker>
           </View>
         </View>
@@ -257,13 +321,18 @@ const newRequirement = () => {
     </ScrollView>
 			<TouchableOpacity
 				style={[styles.submitButton, {
-          backgroundColor: direccion.length < 1 ? 'gray' : 'green'
+          width: '90%',
+          borderWidth: 1,
+          borderColor: (!isButtonEnabled) ? 'gray' : 'green',
+          margin: 10
         }]}
 				onPress={sendRequirement}
-        disabled={direccion.length < 1}
+        disabled={!isButtonEnabled}
 			>
 				<Text
-					style={styles.submitButtonText}
+					style={[styles.submitButtonText, {
+            textAlign: 'center'
+          }]}
 				>
 					Hacer requerimiento
 				</Text>
@@ -275,7 +344,6 @@ const newRequirement = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingHorizontal: 20,
     paddingTop: 50,
     backgroundColor: '#1e2124',
     alignItems: 'center',
@@ -300,7 +368,7 @@ const styles = StyleSheet.create({
 		marginTop: 8,
 		padding: 10,
 		borderRadius: 8,
-		color: 'white'
+		color: 'white',
 	},
 	addButton: {
 		backgroundColor: 'green',
@@ -323,7 +391,6 @@ const styles = StyleSheet.create({
 		fontWeight: 'bold'
 	},
 	submitButton: {
-		backgroundColor: 'blue',
 		padding: 10,
 		borderRadius: 8,
 		marginTop: 10
